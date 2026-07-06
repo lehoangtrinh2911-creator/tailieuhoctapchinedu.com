db36296623bce39753e61ada8e3ba2cb6accd93b
lehoangtrinh2911-creator <lehoangtrinh2911@gmail.com>
2026-07-07 00:03:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Lấy thông tin của nút bấm thông qua ID
+const button = document.getElementById('magic-btn');
+
+// Lắng nghe hành động khi người dùng click chuột vào nút
+button.addEventListener('click', () => {
+    alert('Chúc bạn có một ngày học tập thật năng suất và đạt điểm tối đa nhé! 🚀');
+});
